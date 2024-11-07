8726c6fbe36fc809ae911da04c9d0b91056b1430
Dhiyaay <dhiyaulhaq158@gmail.com>
2024-11-07 10:03:56 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'crud_mhsw',
+  webDir: 'www'
+};
+
+export default config;
